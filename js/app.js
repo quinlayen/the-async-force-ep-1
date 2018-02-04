db97f8beb90206d341fd3dbc055717dd39f0f35e
quinlayen <quinlayen@gmail.com>
2018-02-03 16:26:06 -1000

    single planet displaying but attaching to every instance of film.  Need to create helper to dynamically generate planets and then apply to proper film

--- js/app.js
+++ js/app.js
@@ -73,19 +73,36 @@ modulePerson14();
 function filmListModule(){
 
     const oReq = new XMLHttpRequest();
-
+    //list out the titles of the movies
     function reqListener(){
         const data = JSON.parse(this.response);
-        console.log(data.results[0].planets);
+        //console.log(data.results[0].planets);
         const filmList = document.getElementById('filmList');
 
         const mappedList = data.results.map(function(element){
         let film = document.createElement('li');
         film.innerHTML=element.title
         filmList.appendChild(film);
-        });
+   
+        const oReq2 = new XMLHttpRequest();
+     //list the planets that were in the movie
+            function reqListener2(){
+                //console.log(this.response);
+                const data2 = JSON.parse(this.response);
+                const planetList = document.createElement('ul');
+                const planet = document.createElement('li');
+                planet.innerHTML=data2.name;
+                film.appendChild(planetList);
+                planetList.appendChild(planet);
+                console.log(element)
+            };//reqListener2 close
+        
+            oReq2.addEventListener('load', reqListener2);
+            oReq2.open('GET', "https://swapi.co/api/planets/2/")
+            oReq2.send();
+        });//mappedList close
+        
     };
-
 
     oReq.addEventListener('load',reqListener);
     oReq.open('GET', 'https://swapi.co/api/films/');
