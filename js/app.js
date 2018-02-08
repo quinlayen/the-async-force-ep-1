cbd4d37d356503ecad56628449265c48400231a3
quinlayen <quinlayen@gmail.com>
2018-02-07 21:22:53 -1000

    completed refactoring

--- js/app.js
+++ js/app.js
@@ -2,23 +2,25 @@ console.log('sanity check');
 
 function request(method, url, callback) {
   const oReq = new XMLHttpRequest();
-  oReq.addEventListener('load', callback);
+  oReq.addEventListener('load', function() {
+    const data = JSON.parse(this.responseText);
+    return callback.bind(this)(data);
+  });
   oReq.open(method, url);
   oReq.send();
+  return oReq;
 }
 
 const getUrl = (category, categoryId) =>
   `https://swapi.co/api/${category}/${categoryId}`;
 
 function modulePerson4() {
-  request('GET', getUrl('people', '4'), function() {
-    const data = JSON.parse(this.responseText);
+  request('GET', getUrl('people', '4'), function(data) {
     const person4Name = data.name;
     document.getElementById('person4Name').innerHTML = person4Name;
 
-    request('GET', getUrl('planets', '4'), function() {
-      const data2 = JSON.parse(this.responseText);
-      const person4Homeworld = data2.name;
+    request('GET', getUrl('planets', '4'), function(data) {
+      const person4Homeworld = data.name;
       document.getElementById('person4HomeWorld').innerHTML = person4Homeworld;
     });
   });
@@ -26,14 +28,12 @@ function modulePerson4() {
 modulePerson4();
 
 function modulePerson14() {
-  request('GET', getUrl('people', '14'), function() {
-    const data = JSON.parse(this.responseText);
+  request('GET', getUrl('people', '14'), function(data) {
     const name = data.name;
     document.getElementById('person14Name').innerHTML = name;
 
-    request('GET', getUrl('species', '14'), function() {
-      const data2 = JSON.parse(this.responseText);
-      const person14Species = data2.name;
+    request('GET', getUrl('species', '14'), function(data) {
+      const person14Species = data.name;
       document.getElementById('person14Species').innerHTML = person14Species;
     });
   });
@@ -43,44 +43,28 @@ modulePerson14();
 function filmListModule() {
   //array to hold planet lists
 
-  
   //list out the titles of the movies
-  request('GET', getUrl('films'), function() {
-    const data = JSON.parse(this.response);
+  request('GET', getUrl('films', ''), function(data) {
     const filmList = document.getElementById('filmList');
+
     const mappedList = data.results.map(function(element) {
       let film = document.createElement('li');
       film.innerHTML = element.title;
       filmList.appendChild(film);
       const planetArray = element.planets;
-      const mappedPlanets = element.planets.map(function(items) {
-        createRequest();
-
+      const mappedPlanets = element.planets.map(function(url) {
         //list the planets that were in the movie
-        function createRequest() {
-          const oReq2 = new XMLHttpRequest();
 
-          function reqListener2() {
-            const data2 = JSON.parse(this.response);
-            const planetList = document.createElement('ul');
-            const planet = document.createElement('li');
-            planet.innerHTML = data2.name;
-            film.appendChild(planetList);
-            planetList.appendChild(planet);
-          }
-
-          oReq2.addEventListener('load', reqListener2);
-          oReq2.open('GET', items);
-          oReq2.send();
-        }
-        return items;
+        request('GET', url, function(data) {
+          const planetList = document.createElement('ul');
+          const planet = document.createElement('li');
+          planet.innerHTML = data.name;
+          film.appendChild(planetList);
+          planetList.appendChild(planet);
+        });
       }); //mappedPlanets close
       return element;
     }); //mappedList close
-  }
-
-  oReq.addEventListener('load', reqListener);
-  oReq.open('GET', 'https://swapi.co/api/films/');
-  oReq.send();
+  });
 }
 filmListModule();
